b3869ea08193947b80b1985c445f100b010fc0a5
Ted Havelka <thavelka@cornellpump.com>
2022-08-30 22:48:21 -0700

    Adding device node for ST32WL55JC usart1, flags for LED and usart1 device statae in main() of main.c.

--- zephyr-nucleo-wl55jc2/src/main.c
+++ zephyr-nucleo-wl55jc2/src/main.c
@@ -4,10 +4,18 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+
+
 #include <zephyr/zephyr.h>
 
-
 #include <zephyr/drivers/gpio.h>
+
+
+
+#include "return-values.h"
+
+
+
 
 //#define LED0_NODE DT_ALIAS(led0)
 //#define LEDBLUE_NODE DT_ALIAS(ledblue)
@@ -50,10 +58,47 @@ static const struct device *dev;
 static const struct device *dev_led_blue;
 
 
+
+// - DEV 0830 BEGIN -
+
+static const struct device *dev_st_usart1;
+
+// - DEV 0830 END -
+
+
+
+
+uint32_t initialize_st_usart1(void)
+{
+    if ( dev_st_usart1 == NULL )
+    {
+//        dev_st_usart1 = device_get_binding(DT_LABEL(DT_NODELABEL("usart1")));
+        dev_st_usart1 = device_get_binding(DT_LABEL(DT_NODELABEL(usart1)));
+    }
+
+    if ( dev_st_usart1 != NULL )
+    {
+        return Z__INFO__DEVICE_ALREADY_INITIALIZED_OR_NOT_NULL;
+    }
+    else
+    {
+        return Z__WARNING__NULL_DEVICE_POINTER;
+    }
+}
+
+
+
+
 void main(void)
 {
+// - VAR BEGIN -
+    uint32_t flag__led_green_ok = 1;
+    uint32_t flag__led_blue_ok = 1;
+    uint32_t flag__usart1_ok = 1;
     uint32_t led_is_on = 0;
-    uint32_t rstatus = 0;
+    uint32_t rstatus = ROUTINE_OK;
+// - VAR END -
+
 
     printk("Hello World! %s\n", CONFIG_BOARD);
 
@@ -61,22 +106,38 @@ void main(void)
 
     dev = device_get_binding(LED0_LABEL);
     if (dev == NULL) {
-        return;
+//        return;
+        flag__led_green_ok &= 0;
     }
 
-    rstatus = gpio_pin_configure(dev, LED0_PIN, GPIO_OUTPUT_ACTIVE | LED0_FLAGS);
-    if (rstatus < 0) {
-        return;
+    if ( flag__led_green_ok )
+    {
+        rstatus = gpio_pin_configure(dev, LED0_PIN, GPIO_OUTPUT_ACTIVE | LED0_FLAGS);
+        if (rstatus < 0) {
+//            return;
+            flag__led_green_ok &= 0;
+        }
     }
 
     dev_led_blue = device_get_binding(LED1_LABEL);
     if (dev_led_blue == NULL) {
-        return;
+//        return;
+        flag__led_blue_ok &= 0;
     }
 
-    rstatus = gpio_pin_configure(dev_led_blue, LED1_PIN, GPIO_OUTPUT_ACTIVE | LED1_FLAGS);
-    if (rstatus < 0) {
-        return;
+    if ( flag__led_blue_ok )
+    {
+        rstatus = gpio_pin_configure(dev_led_blue, LED1_PIN, GPIO_OUTPUT_ACTIVE | LED1_FLAGS);
+        if (rstatus < 0) {
+            return;
+        }
+    }
+
+
+    rstatus = initialize_st_usart1();
+    if ( rstatus != Z__INFO__DEVICE_ALREADY_INITIALIZED_OR_NOT_NULL )
+    {
+	printk("- warning - could not initialize STM32WL55JC usart1!\n");
     }
 
 
@@ -87,9 +148,21 @@ void main(void)
 //        rstatus = gpio_pin_toggle_dt(&led);
 //        rstatus = gpio_pin_toggle_dt(&ledblue);
 
-        gpio_pin_set(dev, LED0_PIN, (int)led_is_on);
+        if ( flag__led_green_ok )
+        {
+            gpio_pin_set(dev, LED0_PIN, (int)led_is_on);
+        }
+
         led_is_on = !led_is_on;
-        gpio_pin_set(dev_led_blue, LED1_PIN, (int)led_is_on);
+
+        if ( flag__led_blue_ok )
+        {
+            gpio_pin_set(dev_led_blue, LED1_PIN, (int)led_is_on);
+        }
+
+
+
+
 
         k_msleep(1152);
     }
